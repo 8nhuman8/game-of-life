8a2445ac54c8ee0ffb315b3f676076e7700503bd
Artyom Bezmenov <artem.bezmenov02@gmail.com>
2020-06-19 18:38:58 +0300

    Add grid

--- src/main.py
+++ src/main.py
@@ -1,17 +1,39 @@
-import pygame as pg
+from pygame import init, QUIT
+from pygame import quit as pygame_quit
+from pygame.event import get as get_events
+from pygame.display import set_mode, flip
+from pygame.draw import rect
+from numpy.random import choice
 
 
-pg.init()
+init()
 
+
+WIDTH, HEIGTH = 500, 500
+SCALE = 50
+
+COLS, ROWS = WIDTH // SCALE, HEIGTH // SCALE
+
+grid = choice([True, False], (COLS, ROWS))
+
+screen = set_mode((WIDTH, HEIGTH))
 
 running = True
 
 
 while running:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
+    for event in get_events():
+        if event.type == QUIT:
             running = False
 
+    # for x in grid:
+    #     if cell:
+    #         rect(screen, (255, 255, 255))
+    #     else:
+    #         rect(screen, (0, 0, 0))
 
-pg.quit()
+    flip()
+
+
+pygame_quit()
 quit()
