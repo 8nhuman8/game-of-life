f49d92b1dc2548872c409bec07574321e285be0b
Artyom Bezmenov <artem.bezmenov02@gmail.com>
2020-06-20 22:11:05 +0300

    Add basic algorithm

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 from pygame import init, QUIT
 from pygame import quit as pygame_quit
 from pygame.event import get as get_events
-from pygame.display import set_mode, flip
+from pygame.display import set_mode, set_caption
+from pygame.display import flip
 from pygame.draw import rect
+from pygame.time import Clock
+
+from numpy import zeros
 from numpy.random import choice
 
 
 init()
+set_caption('Game of Life')
 
 
 WIDTH, HEIGTH = 500, 500
@@ -18,8 +23,33 @@ ROWS = HEIGTH // SCALE
 grid = choice([True, False], (COLS, ROWS))
 
 screen = set_mode((WIDTH, HEIGTH))
+clock = Clock()
 
 running = True
+
+
+def count_neighbors(grid, x, y):
+    sum = -int(grid[x, y])
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            col = (x + i + COLS) % COLS
+            row = (x + j + ROWS) % ROWS
+            sum += int(grid[col, row])
+    return sum
+
+def next_generation(grid):
+    next = zeros((COLS, ROWS), bool)
+    for x in range(COLS):
+        for y in range(ROWS):
+            state = grid[x, y]
+            neighbors = count_neighbors(grid, x, y)
+            if not state and neighbors == 3:
+               next[x, y] = True
+            elif state and (neighbors < 2 or neighbors > 3):
+               next[x, y] = False
+            else:
+               next[x, y] = state
+    return next
 
 
 while running:
@@ -41,7 +71,10 @@ while running:
                     )
                 )
 
+    grid = next_generation(grid)
+
     flip()
+    clock.tick(60)
 
 
 pygame_quit()
